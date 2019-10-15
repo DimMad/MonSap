7557cca533a8204c679ab8ae38d2396283ad2c2d
maddim <dimos.madarakis@outlook.com>
2019-10-16 08:21:07 +0900

    Setting up tests

---
+++ app/src/test/java/test/techtest/moneysapling/useraccountsummary/UserAccountSummaryViewModelTest.kt
@@ -0,0 +1,17 @@
+package test.techtest.moneysapling.useraccountsummary
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import test.techtest.moneysapling.data.source.UserAccountSummaryRepository
+
+/**
+ * Unit tests for [UserAccountSummaryViewModel]
+ */
+@ExperimentalCoroutinesApi
+class UserAccountSummaryViewModelTest {
+
+    // Our unit for this test
+    private lateinit var userAccountSummaryViewModel: UserAccountSummaryViewModel
+
+    private lateinit var repository: UserAccountSummaryRepository
+
+}
