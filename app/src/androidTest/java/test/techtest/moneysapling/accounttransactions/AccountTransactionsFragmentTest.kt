cd5571866340beab61fc92c99346d6f1e1108c79
maddim <dimos.madarakis@outlook.com>
2019-10-16 22:35:18 +0900

    Escaped formatting

--- app/src/androidTest/java/test/techtest/moneysapling/accounttransactions/AccountTransactionsFragmentTest.kt
+++ app/src/androidTest/java/test/techtest/moneysapling/accounttransactions/AccountTransactionsFragmentTest.kt
@@ -3,10 +3,7 @@ package test.techtest.moneysapling.accounttransactions
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.test.espresso.Espresso.onView
-
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isChecked
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
